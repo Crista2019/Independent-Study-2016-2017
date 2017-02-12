f6e2ea0b296d201b980cfaf9fee8da0df20bad7c
Crista Falk <Crista2019@users.noreply.github.com>
2017-02-12 15:32:54 -0800

    Revision of Week 2's Assignment on Stacks

--- Completed-Assignments/Term1/Assignment2Stacks.py
+++ Completed-Assignments/Term1/Assignment2Stacks.py
@@ -36,29 +36,17 @@ class Stack(object):
   def is_empty(self):
     return self.data == [] #return either True or False for an instance of a blank array
 
-    # CASEY: Yup, it was unnecessary; I just wanted to make sure you had the right idea for what was going on here. :-)
-    """if self.data.size() < 1: This states that if there is 1 or more (i.e. any) elements in the stack, the stack is not empty, and it returns False
-      return True               I decided that this was mostly unnecessary code.
-    else:
-      return False"""
-
   def push(self, new_val):
     self.data.append(new_val) #new value (i.e. 'top') will be defined as the last index of the data list (i.e. self.data.size() - 1)
 
   def pop(self):
-    return self.data[self.data.size() - 1] #returns top (last element is considered top) #CASEY: Neeeearly there. Recall that "pop" also mutates the list by removing the "top" from the stack.
-    return self.data.pop() #Is this legal? #CASEY: Haha, technically -- but that's just because Python's lists have lots of nifty features. The line above was what I was leaning towards. :-)
-
-    # CASEY: A way you might do this is as follows (though the "pop" list method that Python gives you is a shortcut as you mentioned above.)
     top = self.data.peek()
     self.data = self.data[:-1]
     return top
 
   def peek(self):
-    return self.data[self.data.size() - 1] #Again, top is the last data element, but it is easier to visualize LIFO that way. The last in is the last index value.
-    # CASEY: One minor thing: "self.data" is a python list so it doesn't have a "`size` method; to get the size of one of those you'd use `len(self.data)`.
+    return self.data[len(self.data) - 1] #Again, top is the last data element, but it is easier to visualize LIFO that way. The last in is the last index value.
 
-#I had to make the test function Python 3 compatable. (Just in the print functions and such. :)
 def run_tests():
   print("\nRunning tests!... \n")
   s = Stack()
@@ -86,35 +74,19 @@ run_tests()
 COMPUTATIONAL COMPLEXITY OF STACK OBJECT'S METHODS:
 
 .size()
-  O(n). This method instrinsically depends on the quantity of elements in the stack of data. As a result, the return values varies.
-  #CASEY: If the internal data in our Stack was stored as a LinkedList, then you're right that we'd need to check to see how many values
-          were contained in a one-by-one method. However, since we're using the Python list's "len" method, we should see the computational
-          complexity of *that* method: https://wiki.python.org/moin/TimeComplexity . It turns out, it can do it in O(1) -- aka, "constant" time!
-          It does that by keeping a "counter" of how many values it already contains; that way, it doesn't need to count everything every time
-          we ask because it already has the answer.
+  O(1). Rather than iterating through every value, getting the length of a data set returns the already known sum of its elements.
 
 .isEmpty()
-  O(1). This method just checks to see if the stack is a black list. That is the most work it will do because computers are lazy that way.
-  #CASEY: Yup! If by "black" list here you mean an "empty" list. I've never seen it referred to as a "black" list before. :-)
+  O(1). This method just checks to see if the stack is an empty list. That is the most work it will do because computers are lazy that way.
 
 .push()
   O(1). Regardless of stack size (# of data elements), the implementation will always perform this method in the same amount of time.
-        The new element is just getting added (appended) to the top
-  #CASEY: Yup! And since we're using Python's "append" method inside of our Stack's "push" method, let's refer to that
-          documentation: https://wiki.python.org/moin/TimeComplexity .
+        The new element is just getting pushed to the top
 
 .pop()
-  O(1). Regardless of stack size (# of data elements), the implementation will always perform this method in the same amount of time.
-        ^Same as .push();
-  #CASEY: Yup! Be careful though, as there are *some* caveats to Python's list's "pop" method:
-          http://stackoverflow.com/questions/195625/what-is-the-time-complexity-of-popping-elements-from-list-in-python
-          If you implement it without the "pop" as I wrote above, you'd actually be creating a new "slice", which is O(k) where k is
-          the size of the new slice.
-
+  O(k). k = size of new slice. Because the data is getting shifted as a result ok this implementation of pop(), it considers it as .pop(n) where n = the specified index instead of .pop(), which assumes the last index as the value getting removed.
+ 
 .peek()
   O(1). Regardless of stack size (# of data elements), the implementation will always perform this method in the same amount of time.
-        ^Same as .pop(); However, it is only 'accessing' and not 'mutating' the top value
-  #CASEY: Yup! Note that indexing a list (ie, which is the same as using the "get" method of the list) is O(1), so we're verified:
-          https://wiki.python.org/moin/TimeComplexity#list .
-
+        ^Same as .pop(). Indexing = get() is only accessing 1 specific value 1 time.
 """
