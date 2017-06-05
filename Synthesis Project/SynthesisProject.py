8342c8f37b30e659887373176879263055b9068d
Crista Falk <Crista2019@users.noreply.github.com>
2017-06-05 09:52:59 -0700

    Update SynthesisProject.py
    Eliminated a majority of the code's assignment errors and redundency (if/else conditionals replaced with more general and all-encompassing statements)

--- Synthesis Project/SynthesisProject.py
+++ Synthesis Project/SynthesisProject.py
@@ -9,7 +9,62 @@ def help():
   print("         ##     ## ##       ##       ##        ")
   print("         ##     ## ######## ######## ##        ")
   print('\n')
-  print("")
+  print("Welcome to the help menu.")
+  print("List of useful functions/methods to call and navigate this tool!:")
+  print('\n')
+  
+  print("list_classes():", '\n', 'Fuction; Lists all possible classes that can be used in a typical D&D character.')
+  
+  print('\n')
+  
+  print("list_races():", '\n', "Function; Lists all possible races that can be used in a typical D&D character. ")
+  
+  print('\n')
+  
+  print('displayPartyMembers():', '\n', "Function; Prints all current members of a party. ")
+  
+  print('\n')
+  
+  print("printAllMaps():", '\n', "Function; prints all maps that have been created since the initializing the program.")
+      
+  print('\n')
+  
+  print("createNewPlayer():", '\n', "Function; One of the most useful tools to create a new player step by step.")
+  
+  print('\n')
+  
+  print("createNewMap():", '\n', "Function; Interactive step-by-step map maker")
+  
+  print('\n')
+  
+  print("createNewCampain():", '\n', "Function; Create a campain by adding potential quest branches that emulate a 'choose your own adventure' style structure, primarily to assist the dungeon master through DMing")
+  
+  print('\n')
+  
+  print("Player(object):", '\n', "Class; contains data and methods for defining each player in a party.(PRIMARY DATA STRUCTURE: HASH MAP)")
+  
+  print('\n')
+  
+  print("Map(object):", '\n', "Class; contains basic map information as well as all the villages and connecting villages of a given map and methods for manipulating this data. (PRIMARY DATA STRUCTURE: GRAPH) ")
+  
+  print('\n')
+  
+  print("Quest(object):", '\n', "Class; contains methods (PRIMARY DATA STRUCTURE: BINARY TREE)")
+  
+  print('\n')
+  
+  print("", '\n', "")
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 possible_classes = ["barbarian", "bard", "cleric", "druid", "fighter", "mage", "monk", "paladin", "ranger", "sorcerer", "rogue", "warlock"]
 
@@ -21,10 +76,98 @@ possible_races = ["human", "elf", "dwarf", "halfling", "gnome", "half-orc"]
 def list_races():
 	print(possible_races)
 	
-max_players = ["player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8", "player9", "player10"]
+players = []
+
+def playerIcon1():
+  print('\n')
+  print("        ,.,")
+  print("       ((())")
+  print("      (|*_*|)")
+  print("       c\=/c")
+  print('\n')
+  
+def playerIcon2():
+  print('\n')
+  print("      .-.")
+  print("     (~ ~)")
+  print("     :o o:")
+  print("    (((_)))")
+  print("      '-'")
+  print('\n')
+  
+def playerIcon3():
+  print('\n')
+  print("       ,")
+  print("   ,.'` `'.,")
+  print("    |:o o:|")
+  print("     \(_)/")
+  print('\n')
+  
+def playerIcon4():
+  print('\n')
+  print("       ,")
+  print("   ,.'` `'.,")
+  print("    |:o o:|")
+  print("     \(_)/")
+  print('\n')
+      
+def playerIcon5():
+  print("       ,")
+  print("     ,iIi,")
+  print("    (((()))")
+  print("    ))o_o((")
+  print("     '\=/'")
+  print('\n')
+      
+def playerIcon6():
+  print('\n')
+  print("      ,.,")
+  print("    ((~'~))")
+  print("   '(|o_o|)'")
+  print("   ,..\=/..,")
+  print('\n')
+      
+def playerIcon7():
+  print('\n')
+  print("    (\___/)")
+  print("     )o o(")
+  print("    (_(_)_)")
+  print('\n')
+      
+def playerIcon8():
+  print('\n')
+  print("    (@@@@@)")
+  print("    @)0 0(@")
+  print("    @((_))@")
+  print("      )=(")
+  print('\n')
+      
+def playerIcon9():
+  print('\n')
+  print("      ~I~")
+  print("     ('¯`)")
+  print("     )Ø Ø(")
+  print("    ( (_) )")
+  print("     `'-'`")
+  print('\n')
+      
+def playerIcon10():
+  print('\n')
+  print("      ,.,")
+  print("     (~ ~)")
+  print("    q:0 0:p")
+  print("     ((_))")
+  print("      'u'")
+  print('\n')
+
+maps = []
+
+def printAllMaps():
+  for x in range(0, len(maps)):
+    print(maps[x].name)
 
 def displayPartyMembers():
-  print("Party:", Player.teamList, "(Party size: {})".format(Player.teamCount))
+  print("Party:", players, "(Party size: {})".format(Player.teamCount))
 
 def createNewPlayer():
   if input("Do you wish to create a new player? (y/n)").lower() == 'y':
@@ -32,7 +175,7 @@ def createNewPlayer():
     
     print('\n')
     name = input("What is the the new character's name?")
-    if name in Player.teamList.keys():
+    if name in players:
       print("Sorry, that name is taken. Please choose a name not already present within your party.")
       name = input("What is the the new character's name?")
     
@@ -44,139 +187,15 @@ def createNewPlayer():
     print("           \_'_'_\/")
     print('\n')
     
-    n = Player.teamCount
-    if n == 0:
-      player1 = Player(name)
-      player1.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player1.")
-      
-      print('\n')
-      print("        ,.,")
-      print("       ((())")
-      print("      (|*_*|)")
-      print("       c\=/c")
-      print('\n')
-      
-      player1.displayCharacter()
-    elif n == 1:
-      player2 = Player(name)
-      player2.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player2.")
-      
-      print('\n')
-      print("      .-.")
-      print("     (~ ~)")
-      print("     :o o:")
-      print("    (((_)))")
-      print("      '-'")
-      print('\n')
-      
-      player2.displayCharacter()
-    elif n == 2:
-      player3 = Player(name)
-      player3.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player3.")
-      print('\n')
-      print("       ,")
-      print("   ,.'` `'.,")
-      print("    |:o o:|")
-      print("     \(_)/")
-      print('\n')
-      player3.displayCharacter()
-    elif n == 3:
-      player4 = Player(name)
-      player4.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player4.")
-      print('\n')
-      print("       ,")
-      print("     ,iIi,")
-      print("    (((()))")
-      print("    ))o_o((")
-      print("     '\=/'")
-      print('\n')
-      player4.displayCharacter()
-    elif n == 4:
-      player5 = Player(name)
-      player5.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player5.")
-      print('\n')
-      print("      ,.,")
-      print("    ((~'~))")
-      print("   '(|o_o|)'")
-      print("   ,..\=/..,")
-      print('\n')
-      player5.displayCharacter()
-    elif n == 5:
-      player6 = Player(name)
-      player6.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player6.")
-      print('\n')
-      print("    (\___/)")
-      print("     )o o(")
-      print("    (_(_)_)")
-      print('\n')
-      player6.displayCharacter()
-    elif n == 6:
-      player7 = Player(name)
-      player7.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player7.")
-      print('\n')
-      print("    (@@@@@)")
-      print("    @)0 0(@")
-      print("    @((_))@")
-      print("      )=(")
-      print('\n')
-      player7.displayCharacter()
-    elif n == 7:
-      player8 = Player(name)
-      player8.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player8.")
-      print('\n')
-      print("      ~I~")
-      print("     ('¯`)")
-      print("     )Ø Ø(")
-      print("    ( (_) )")
-      print("     `'-'`")
-      print('\n')
-      player8.displayCharacter()
-    elif n == 8:
-      player9 = Player(name)
-      player9.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player9.")
-      print('\n')
-      print("      ,.,")
-      print("     ((())")
-      print("    (|o_o|)")
-      print("    ()\=/()")
-      print("    ()\_/()")
-      print('\n')
-      player9.displayCharacter()
-    elif n == 9:
-      player10 = Player(name)
-      player10.init_inventory()
-      print('\n')
-      print(name, " will be refered to as player10.")
-      print('\n')
-      print("      ,.,")
-      print("     (~ ~)")
-      print("    q:0 0:p")
-      print("     ((_))")
-      print("      'u'")
-      print('\n')
-      player10.displayCharacter()
-    else:
-      print("The party has exceeded the max amount of manageable players. No more than ten people per campain.")
+    
+    n = len(players)
+    players.append(Player(name))
+    players[n].init_inventory()
     print('\n')
-    print("At any time, call player[number].displayCharacter() to check your player's stats.")
+    print('\n')
+    players[n-1].displayCharacter()
+    print(name, " will be refered to as players[{}].".format(n))
+    print("At any time, call players[number].displayCharacter() to check your player's stats. Make sue to start at 0!")
 
   else:
     print("Ok. New player request terminated")
@@ -200,6 +219,31 @@ def createNewMap():
     print("          _____|_____")
     print("         /___________\ ")
     
+    map_name = input("What would you like to call this new map?")
+    what = input("What type of map is {}? (e.g. city, world, dungeon, etc)".format(map_name))
+    
+    m = len(maps)
+    maps.append(Map(map_name, what))
+    print(map_name, "will be refered to as maps[{}].".format(m))
+    
+    print('\n')
+    maps[m].init_add_stops()
+    
+    print('\n')
+    maps[m].printMap()
+    
+    print('\n')
+    print("By default, the map is initialized in the 'unlocked' setting. To change this, call maps[number].lockMap() or maps[number].unlockMap()")
+    
+    print('\n')
+    print("To add additional stops at any time, call maps[number].addStop('town name', 'connecting town name')")
+    
+    print('\n')
+    print("To remove stops at any time, call maps[number].remove(town)")
+    
+    print('\n')
+    print("To print a working list of all maps, call printAllMaps()")
+    
   else:
     print("Ok. New map request terminated.")
   
@@ -219,19 +263,43 @@ def createNewCampain():
     print("              ) ) ")
     print("              ^ ^")
     
+    name = input("What shall the title of this campain be?")
+    root = input("What is the inciting incident of the campain?")
+    
+    value1 = input("What is one possible quest that branches from this incident?")
+    value2 = input("What is another possible quest that branches from this incident?")
+    
+    campain = Quest(root)
+    campain.insert_left(value1)
+    campain.insert_right(value2)
+    
+    if input("Do you have any further quests to add at this time? (y/n)") == 'y':
+      add_quest = True
+      
+      while add_quest:
+        if input("Does this quest result from the left or right option? (l/r)") == 'l':
+          campain.quest_choice_left.insert_left(input("What is the first choice?"))
+          campain.quest_choice_left.insert_right(input("what is the second choice?"))
+        else:
+          campain.quest_choice_right.insert_left(input("What is the first choice?"))
+          campain.quest_choice_right.insert_right(input("what is the second choice?"))
+        if input("Continue adding quests? (y/n)") =="n":
+          add_quest = False
+      print("Ok. ", name, " has been created. Check the status of your quest progression by calling campain.print_quests(root)")
   else:
     print("Ok. New campain request terminated.")
 
 class Player(object):
 		'Common base class for all players'
 		teamCount = 0
-		teamList = {}
 
 		def __init__(self, name):
 
 		  self.name = name #Primitive Data Structure: String
 		  
 		  self.race = input("Choose your race. (human, elf, dwarf, halfling, gnome, half-orc)").lower()
+		  
+		  self.icon = input("Choose one of ten player icons [playerIcon1(), playerIcon6(), etc]")
 		  
 		  if self.race not in possible_races:
 		    self.race = input("Please choose a valid race.")
@@ -242,8 +310,7 @@ class Player(object):
 		    self.character_class = input("Please choose a valid class.")
 		    
 		  self.stats = {"Strength": random.randint(3,18), "Constitution": random.randint(3,18), "Dexterity": random.randint(3,18), "Intelligence": random.randint(3,18), "Wisdom": random.randint(3,18), "Charisma": random.randint(3,18)}
-			
-		  Player.teamList[self.name] = max_players[Player.teamCount]
+	
 		  Player.teamCount += 1
 		  self.inventory = set()
 		
@@ -273,7 +340,8 @@ class Player(object):
 			self.displayInventory()
 			print("Class: ", self.character_class)
 			print("Race: ", self.race)
-			print("Member of party:", Player.teamList, "(Party size: {})".format(Player.teamCount))
+			#return self.icon
+			print("Member of party:", players, "(Party size: {})".format(Player.teamCount))
 		
 
 #http://www.python-course.eu/python3_inheritance.php FOR ADDING DIFFERENT PLAYER CLASSES AND RACES IF TIME ALLOWS
@@ -288,6 +356,8 @@ TODO:
 
 class Map(object):
   """ Graph data structure, undirected by default. """
+  mapCount = 0 
+  
   def __init__(self, name, what, directed=False, locked=False): 
     #what refers to whether the map is of the game world, dungeon, room, etc
     #connections refers to the connection between nodes
@@ -298,6 +368,8 @@ class Map(object):
     #A SET of DICTIONARIES in which each value in the key is the town name and the value is the conecting town
     self._directed = directed
     self.locked = bool(locked)
+    
+    Map.mapCount += 1
 
     #HERE IS WHERE I WILL ADD THE NODES THAT REPRESENT DIFFERENT VISITABLE LOCATION WITHIN THE MAP, AS WELL AS EDGES
     #THIS IS GOING TO BE IN THE FORM OF A UNDIRECTED, WEIGHTED GRAPH!
@@ -306,12 +378,29 @@ class Map(object):
 
   def lockMap(self):
     self.locked = True
+  
+  def printMap(self):
+    print(self.name, " is a ", self.what, " map.")
+    if self.locked:
+      print("It is currently locked.")
+    else:
+      print("It is currently unlocked.")
+    print(self.stops)
 
-  def addStop(self, stop): 
-    #"stop" input should be in the form of a TUPLE then will be turned into a DICTIONARY; stop = (town, connecting_town) then code => {town: connecting_town)
+  def init_add_stops(self):
+    if input("Would you like to add a new stop to {}? (y/n)".format(self.name)) == 'y':
+        add_stops = True
+        while add_stops:
+          town = input("What is the name of this stop?")
+          connecting_town = input("What town is {} connected to?".format(town))
+          self.addStop(town, connecting_town)
+          if input("Would you like to add another stop? (y/n)") == 'n':
+            add_stops = False
+  
+  def addStop(self, town, connecting_town): 
+    #"stop" input should be in the form of a TUPLE then will be turned into a DICTIONARY; stop = (town, connecting_town) then code => {town: connecting_town}
     if self.locked == False:
-      for town, connecting_town in stop:
-            self.stops[town] = connecting_town
+      self.stops[town] = connecting_town
       print("Modified map of", self.name, self.stops)
     else:
       print("The", self.name, "is LOCKED. DM must reset the value of self.locked to True to unlock!")
@@ -320,11 +409,9 @@ class Map(object):
     if self.locked != False:
       print("The", self.name, "is LOCKED. DM must reset the value of self.locked to True to unlock!")
     else:
-      for x in self.stops:
-        if x == node:
-          #NEXT STEP IS TO DELETE X IF IT IS FOUND
-        #ELSEIF X == ONE OF THE VALUES RATHER THAN KEYS, DELETE CONNECTION
-          pass
+      if node in self.stops: 
+        del self.stops[node]
+
 """
   def is_connected(self, node1, node2):
     # Is node1 directly connected to node2
@@ -347,33 +434,26 @@ class Map(object):
 """
 
 class Quest(object):
-  #binary search tree
+  #binary tree
   def __init__(self, val):
-      self.quest_choice_one = None
-      self.quest_choice_two = None
+      self.quest_choice_left = None
+      self.quest_choice_right = None
       self.data = val
 
-  def binary_insert(self, root, node):
-    if root is None:
-        root = node
-    else:
-        if root.data > node.data:
-            if root.quest_choice_one is None:
-                root.quest_choice_one = node
-            else:
-                self.binary_insert(root.quest_choice_one, node)
-        else:
-            if root.quest_choice_two is None:
-                root.quest_choice_two = node
-            else:
-                self.binary_insert(root.quest_choice_two, node)
+  def insert_left(self, val):
+    self.quest_choice_left = Quest(val)
+    return self.quest_choice_left
+    
+  def insert_right(self, val):
+    self.quest_choice_right = Quest(val)
+    return self.quest_choice_right
   
   def print_quests(self, root):
     if not root:
         return        
     print(root.data)
-    self.print_quests(root.quest_choice_one)
-    self.print_quests(root.quest_choice_two)   
+    self.print_quests(root.quest_choice_left)
+    self.print_quests(root.quest_choice_right)
     
 """
 me = Player("Crista")
@@ -404,3 +484,89 @@ print("            <_n_                     __-^-_    `)  \-.______________,-~' 
 print("             `B'\)                  ///,-'~`__--^-  |-------~~~~^' ")
 print("             /^>                           ///,--~`-\ ")
 print("            `  `                                     ")
+
+
+"""
+
+class Dog(object):
+  def __init__(self, name, size, cute):
+    self.name = name
+    self.size = size
+    self.cute = cute
+   
+  def bark(self):
+    print "Bark!"
+    
+  def getName(self):
+    return self.name
+  
+dogs = []
+
+def add_dog(name, size, cute):
+  dogs.append( Dog(name, size, cute) )
+
+for name in ["Bill", "Frank", "Trogdor", "Hercules", "Betty"]:
+  add_dog(name, 10, True)
+
+dogs[0] # Player 1
+dogs[1] # Player 2
+dogs[2] # Player 3, etc.
+
+####### ####### ####### ####### ####### #######
+
+for ITEM in THINGS:
+  # ...
+
+for (a,b) in [(C,D)]:
+  # ...
+  
+for (a,b) in (C,D):
+  # Oh noez!
+  
+def do_something(tup):
+  my_dict = {}
+  my_dict[tup[0]] = tup[1]
+
+def do_something(a, b):
+  # ...
+
+do_something(*(a,b))
+
+
+####### ####### ####### ####### ####### #######
+
+class BinaryTree(object):
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+   
+  def insert_left(self, val):
+    self.left = BinaryTree(val)
+    
+  def insert_right(self, val):
+    self.right = BinaryTree(val)
+  
+  def __str__(self):
+    return "{}({}, {})".format(self.val, self.left, self.right)
+    
+tree = BinaryTree("Animals")
+tree.insert_left("Dog")
+tree.insert_right("Cat")
+tree.left.insert_left("Shnouzer")
+tree.left.insert_right("Poodle")
+tree.left.right.insert_left("Golden Doodlie thing.")
+print tree.left.right.left.val
+
+              Animals
+              /    \
+            Dog     Cat
+           /   \
+     Shnouzer  Poodle
+                /
+          Golden Doodlie Thing
+
+
+print tree #Expect "Animals(Dog( ... ), Cat(None, None))
+
+"""
